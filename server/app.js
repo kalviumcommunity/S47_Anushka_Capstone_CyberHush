7ba76cf14c3396310ac73cba7c814dffcc18a9ed
Anushka Poonia <141112216+anushkapoonia07@users.noreply.github.com>
2024-03-04 16:05:34 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Basic server setup')
