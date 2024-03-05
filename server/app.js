5bd16d32a851c3408c9b349f007d40286c5949e9
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-03-05 15:01:06 +0530

    Basic frontend setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 // const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Basic server setup')
