b684b0573b48d4a0157170eb943c6654d7b5624d
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-03-19 15:40:47 +0530

    Created frontend component in react for about page

--- client/src/LandingPage/Homepage.jsx
+++ client/src/LandingPage/Homepage.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 function Home(){
   return (
     <>
-    <navbar className="navbar">
+    <header className="navbar">
       <div className="logo">Logo</div>
       <div className="nav-links">
-        <a href="#">About</a>
+        <Link to="/about">About</Link>
         <a href="#">Contact</a>
         <a href="#">FAQ</a>
         <Link to="/signup">SignUp</Link>
       </div>
-    </navbar>
+    </header>
     <div className="main">
       <h1>CyberHusk</h1>
       <p>A good act does not wash out the bad, nor a bad act the good. Each should have its own reward</p>
