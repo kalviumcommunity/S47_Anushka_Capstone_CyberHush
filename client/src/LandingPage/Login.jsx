2fb7e5dbc94635bcad166797e8d7e3bb0aad8477
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-03-22 16:21:06 +0530

    added the component of home page which is going to appear after successful login

--- client/src/LandingPage/Login.jsx
+++ client/src/LandingPage/Login.jsx
@@ -29,11 +29,9 @@ function Login() {
       <header className="navbar">
         <div className="logo">Logo</div>
         <div className="nav-links">
-          <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/faq">FAQ</Link>
           <Link to="/contact">Contact</Link>
-          <Link to="/login">Login</Link>
         </div>
       </header>
       <div className="login-container">
